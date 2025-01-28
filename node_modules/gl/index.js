81d84fd894f668763fde45b5157d03f1adfb6e7d
zcj <1491025307@qq.com>
2025-01-28 17:57:45 +0800

    删除

--- node_modules/gl/index.js
+++
@@ -1,6 +0,0 @@
-if (typeof WebGLRenderingContext !== 'undefined') {
-  module.exports = require('./src/javascript/browser-index')
-} else {
-  module.exports = require('./src/javascript/node-index')
-}
-module.exports.WebGLRenderingContext = require('./src/javascript/webgl-rendering-context').WebGLRenderingContext
